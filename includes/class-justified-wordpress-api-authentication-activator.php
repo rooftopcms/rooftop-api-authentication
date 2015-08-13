5f66ef1fc0c537307a957570eee9918fd15f5b07
Paul Hendrick <paul@errorstudio.co.uk>
2015-08-13 13:14:17 +0100

    initial commit - boilerplate plugin

---
+++ includes/class-justified-wordpress-api-authentication-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://errorstudio.co.uk
+ * @since      1.0.0
+ *
+ * @package    Justified_Wordpress_Api_Authentication
+ * @subpackage Justified_Wordpress_Api_Authentication/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Justified_Wordpress_Api_Authentication
+ * @subpackage Justified_Wordpress_Api_Authentication/includes
+ * @author     Error Studio <info@errorstudio.co.uk>
+ */
+class Justified_Wordpress_Api_Authentication_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-justified-wordpress-api-authentication-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://errorstudio.co.uk
+ * @since      1.0.0
+ *
+ * @package    Justified_Wordpress_Api_Authentication
+ * @subpackage Justified_Wordpress_Api_Authentication/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Justified_Wordpress_Api_Authentication
+ * @subpackage Justified_Wordpress_Api_Authentication/includes
+ * @author     Error Studio <info@errorstudio.co.uk>
+ */
+class Justified_Wordpress_Api_Authentication_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
